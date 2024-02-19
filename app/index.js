91703ac0e47126b48e295259b1178736a404eabc
martinromario55 <martinromario55@gmail.com>
2024-02-19 18:37:14 +0300

    Installing packages and setting up nativewind

---
+++ app/index.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function Index() {
+  return (
+    <View className="bg-rose-500 flex-1 justify-center items-center">
+      <Text>Index</Text>
+    </View>
+  )
+}
