bacf9afd164ea5e2a160efc4bdde471ec5e1cfa7
martinromario55 <martinromario55@gmail.com>
2024-02-22 13:21:05 +0300

    Chat Room Screen design

--- app/(app)/chatRoom.js
+++ app/(app)/chatRoom.js
@@ -1,13 +1,47 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import { useLocalSearchParams } from 'expo-router'
+import { View, Text, TextInput, TouchableOpacity } from 'react-native'
+import React, { useState } from 'react'
+import { router, useLocalSearchParams } from 'expo-router'
+import { StatusBar } from 'expo-status-bar'
+import ChatRoomHeader from '../../components/ChatRoomHeader'
+import MessageList from '../../components/MessageList'
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen'
+import { Feather } from '@expo/vector-icons'
+import CustomKeyboardView from '../../components/CustomKeyboardView'
 
 export default function ChatRoom() {
   const item = useLocalSearchParams()
-  console.log('got items data:', item)
+  const [messages, setMessages] = useState([])
+  // console.log('got items data:', item)
   return (
-    <View>
-      <Text>ChatRoom</Text>
-    </View>
+    <CustomKeyboardView inChat={true}>
+      <View className="flex-1 bg-white">
+        <StatusBar style="dark" />
+        <ChatRoomHeader user={item} router={router} />
+        <View className="h-3 border-b border-neutral-300" />
+
+        <View className="flex-1 justify-betwee overflow-visible">
+          <View className="flex-1">
+            <MessageList messages={messages} />
+          </View>
+        </View>
+
+        <View style={{ marginBottom: hp(1.7) }} className="pt-2">
+          <View className="flex-row mx-2 justify-between p-2 border border-neutral-300 rounded-3xl pl-5">
+            <TextInput
+              placeholder="Type message..."
+              className="flex-1"
+              style={{ fontSize: hp(2) }}
+            />
+
+            <TouchableOpacity className="bg-neutral-200 p-2 mr-[1px] rounded-full">
+              <Feather name="send" size={hp(2.7)} color="#737373" />
+            </TouchableOpacity>
+          </View>
+        </View>
+      </View>
+    </CustomKeyboardView>
   )
 }
--- components/CustomKeyboardView.js
+++ components/CustomKeyboardView.js
@@ -8,13 +8,27 @@ import {
 import React from 'react'
 
 const ios = Platform.OS === 'ios'
-export default function CustomKeyboardView({ children }) {
+export default function CustomKeyboardView({ children, inChat }) {
+  let kavConfig = {}
+  let scrollViewConfig = {}
+  if (inChat) {
+    kavConfig = { keyboardVerticalOffset: 90 }
+    scrollViewConfig = { contentContainerStyle: { flex: 1 } }
+  }
   return (
     <KeyboardAvoidingView
       behavior={ios ? 'padding' : 'height'}
       style={{ flex: 1 }}
+      {...kavConfig}
     >
-      <ScrollView>{children}</ScrollView>
+      <ScrollView
+        style={{ flex: 1 }}
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        {...scrollViewConfig}
+      >
+        {children}
+      </ScrollView>
     </KeyboardAvoidingView>
   )
 }
